38f5a5757a0b74e4392dd23ae53cf8f94db33961
Danni Zapata <zapatadannyeliezer@gmail.com>
2022-09-27 13:59:46 +0200

    80% terminado

--- app.js
+++ app.js
@@ -26,43 +26,254 @@ function initCanvas(){
             h: options.h || '',
             image: options.image || enemiespic1
         }
+        //new enemyTemplate({id: "enemy3", x: 350, y: 50, w: 80, h: 30})
     }
-    let enemis = [
-        new enemyTemplate({id: "enemy3", x: 350, y: 50, w: 80, h: 30})
-    ]
+    let enemies = [
+        new enemyTemplate({id: "enemy1", x: 100, y: -20, w: 50, h: 30 }),
+        new enemyTemplate({id: "enemy2", x: 225, y: -20, w: 50, h: 30 }),
+        new enemyTemplate({id: "enemy3", x: 350, y: -20, w: 80, h: 30 }),
+        new enemyTemplate({id: "enemy4", x:100,  y:-70,  w:80,  h: 30}),
+        new enemyTemplate({id: "enemy5", x:225,  y:-70,  w:50,  h: 30}),
+        new enemyTemplate({id: "enemy6", x:350,  y:-70,  w:50,  h: 30}),
+        new enemyTemplate({id: "enemy7", x:475,  y:-70,  w:50,  h: 30}),
+        new enemyTemplate({id: "enemy8", x:600,  y:-70,  w:80,  h: 30}),
+        new enemyTemplate({id: "enemy9", x:475,  y:-20,  w:50,  h: 30}),
+        new enemyTemplate({id: "enemy10",x: 600, y: -20, w: 50, h: 30}),
 
-    var renderEnemies = function(enemyList){
+        //Segundo grupo de enemigos
+        new enemyTemplate({ id: "enemy11", x: 100, y: -220, w: 50, h: 30, image: enemiespic2 }),
+        new enemyTemplate({ id: "enemy12", x: 225, y: -220, w: 50, h: 30, image: enemiespic2 }),
+        new enemyTemplate({ id: "enemy13", x: 350, y: -220, w: 80, h: 50, image: enemiespic2 }),
+        new enemyTemplate({ id: "enemy14", x: 100, y: -270, w: 80, h: 50, image: enemiespic2 }),
+        new enemyTemplate({ id: "enemy15", x: 225, y: -270, w: 50, h: 30, image: enemiespic2 }),
+        new enemyTemplate({ id: "enemy16", x: 350, y: -270, w: 50, h: 30, image: enemiespic2 }),
+        new enemyTemplate({ id: "enemy17", x: 475, y: -270, w: 50, h: 30, image: enemiespic2 }),
+        new enemyTemplate({ id: "enemy18", x: 600, y: -270, w: 80, h: 50, image: enemiespic2 }),
+        new enemyTemplate({ id: "enemy19", x: 475, y: -200, w: 50, h: 30, image: enemiespic2 }),
+        new enemyTemplate({ id: "enemy20", x: 600, y: -200, w: 50, h: 30, image: enemiespic2 })
+    ];
+
+    let renderEnemies = function(enemyList){
 
         for(let i = 0; i < enemyList.length; i++){
             let enemy = enemyList[i];
             ctx.drawImage(enemy.image, enemy.x, enemy.y += .5, enemy.w, enemy.h);
+            launcher.hitDetectLowerlevel(enemy);
         }
     }
 
-     function launcher(){
+     function Launcher(){
         this.y = 500, 
         this.x = cW*.5-25, 
         this.w = 100, 
         this.h = 100,   
         this.direccion, 
         this.bg="white", 
-    this.misiles = [];
+        this.misiles = [];
+
+        this.gameStatus = {
+            over: false,
+            message: "",
+            fillStyle: 'red',
+            font: 'italic bold 36px Arial, sans-serif',
+        }
 
         this.render = function(){
-            ctx.fillstyle = this.bg;
+            if(this.direccion === 'left'){
+                this.x -= 5;
+            }else if(this.direccion === 'right'){
+                this.x += 5;
+            }else if(this.direccion === 'downArrow'){
+                this.y += 5;
+            }else if(this.direccion === 'upArrow'){
+                this.y -= 5;
+            }
+
+            ctx.fillStyle = this.bg;
             ctx.drawImage(backgroundImage, 10, 10);
             ctx.drawImage(naveImage, this.x, this.y, 100, 90);
+
+            for(let i = 0; i < this.misiles.length; i++){
+                let m = this.misiles[i];
+                ctx.fillRect(m.x, m.y -= 5, m.w, m.h);
+                this.hitDetect(m, i);
+                if(m.y <= 0){
+                    this.misiles.splice(i, 1);
+                }
+            }
+
+            if(enemies.length === 0){
+                clearInterval(animateIntervale);
+                ctx.fillStyle = 'yellow';
+                ctx.font = this.gameStatus.font;
+                ctx.fillText('You win!', cW *.5 - 80, 50);
+            }
         }
+
+        this.hitDetect = function(m, mi){
+            for(let i = 0;  i < enemies.length; i++){
+                let e = enemies[i];
+
+                if(m.x <= e.x + e.w && m.x + m.w >= e.x &&
+                    m.y >= e.y && m.y <= e.y + e.h){
+                    enemies.splice(i, 1);
+                    document.querySelector('.barra'). innerHTML = "Destroyed " + e.id;
+                }
+            }
+        }
+
+        this.hitDetectLowerlevel = function(enemy){
+            if(enemy.y > 550){
+                this.gameStatus.over = true;
+                this.gameStatus.message = 'Enemy(s) have passed!';
+            }
+
+            if((enemy.y < this.y + 25 && enemy.y > this.y - 25) &&
+               (enemy.x < this.x + 45 && enemy.x > this.x - 45)){    
+                this.gameStatus.over = true;
+                this.gameStatus.message = 'You Died!';
+            }
+
+            if(this.gameStatus.over === true){
+                clearInterval(animateIntervale);
+                ctx.fillStyle = this.gameStatus.fillStyle;
+                ctx.font = this.gameStatus.font;
+
+                ctx.fillText(this.gameStatus.message, cW * .5 - 80 , 50);
+            }
+        }
+
    }  
 
-    //let Launcher = new Launcher();
+    let launcher = new Launcher();
     function animate(){
         ctx.clearRect(0, 0, cW, cH);
-        //Launcher.render();
-        renderEnemies(enemis);
+        launcher.render();
+        renderEnemies(enemies);
     }
 
     let animateIntervale = setInterval(animate, 6);
+
+    let left_btn  = document.getElementById('left_btn');
+    let right_btn = document.getElementById('right_btn');
+    let fire_btn  = document.getElementById('fire_btn');
+
+    //Aqui se crean ls direccionales
+
+    document.addEventListener('keydown', function(event){
+        if(event.keyCode === 37){
+            launcher.direccion = 'left';
+            if(launcher.x < cW*.2 -130){
+                launcher.x += 0;
+                launcher.direccion = '';
+            }
+        }
+    });
+
+    document.addEventListener('keyup', function(event){
+        if(event.keyCode === 37){
+            launcher.x += 0;
+            launcher.direccion = '';
+        }
+    });
+
+    document.addEventListener('keydown', function(event){
+        if(event.keyCode === 39){
+            launcher.direccion = 'right';
+            if(launcher.x > cW - 110){
+                launcher.x -= 0;
+                launcher.direccion - '';
+            }
+        }
+    });
+
+    document.addEventListener('keyup', function(event){
+        if(event.keyCode === 39){
+            launcher.x -= 0;
+            launcher.direccion = '';
+        }
+    });
+
+    document.addEventListener('keydown', function(event){
+        if(event.keyCode === 38){
+           launcher.direccion = 'upArrow';
+           if(launcher.y < cH*.2 - 80){
+            launcher.y += 0;
+            launcher.direccion = '';
+           }
+        }
+    });
+
+    //Apartir de aqui abilitamos las direccionales
+
+    document.addEventListener('keyup', function(event){
+        if(event.keyCode === 38){
+            launcher.y -= 0;
+            launcher.direccion = '';
+        }
+    });
+
+    document.addEventListener('keydown', function(event){
+        if(event.keyCode === 40){
+           launcher.direccion = 'downArrow';
+           if(launcher.y > cH - 110){
+            launcher.y -= 0;
+            launcher.direccion = '';
+           }
+          
+        }
+    });
+
+    document.addEventListener('keyup', function(event){
+        if(event.keyCode === 40){
+            launcher.y += 0;
+            launcher.direccion = '';
+        }
+    });
+
+    document.addEventListener('keydown', function(event){
+        if(event.keyCode === 80){
+           location.reload();         
+        }
+    });
+
+    //Aui se abilita el mouse para disparar y moverse
+
+    left_btn.addEventListener('mousedown', function(event){
+        launcher.direccion = 'left';
+    });
+
+    left_btn.addEventListener('mouseup', function(event){
+        launcher.direccion = '';
+    });
+
+    right_btn.addEventListener('mousedown', function(event){
+        launcher.direccion = 'right';
+    });
+
+    right_btn.addEventListener('mouseup', function(event){
+        launcher.direccion = '';
+    });
+
+    fire_btn.addEventListener('mousedown', function(event){
+        launcher.misiles.push({
+            x: launcher.x + launcher.w *.5,
+            y: launcher.y,
+            w: 3,
+            h: 10
+        });
+    });
+
+    document.addEventListener('keydown', function(event){
+        if(event.keyCode === 32){
+            launcher.misiles.push({
+                x: launcher.x + launcher.w *.5,
+                y: launcher.y,
+                w: 3,
+                h: 10
+            });
+        }
+    });
 
 }
 
